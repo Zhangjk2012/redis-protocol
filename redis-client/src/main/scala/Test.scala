5a39ed81bc3716235bafdbd4b87bf901fd810c62
zhangjiangke <zhangjiangke@baidu.com>
2018-06-11 20:02:58 +0800

    just only for commit the src directory. do nothing. these files will be deleted in the future.

---
+++ redis-client/src/main/scala/Test.scala
@@ -0,0 +1,7 @@
+/**
+  *
+  * Created by ZhangJiangke on 2018-06-11 19:53
+  */
+class Test {
+
+}
---
+++ redis-client/src/test/scala/TestTest.scala
@@ -0,0 +1,7 @@
+/**
+  *
+  * Created by ZhangJiangke on 2018-06-11 19:54
+  */
+class TestTest {
+
+}
